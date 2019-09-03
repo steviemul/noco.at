3f21b953f86b84ab393f406e818cdc6cc51a8cf1
steviemul <stephen.mulrennan@ntlworld.com>
2019-09-03 17:49:23 +0100

    Add basic indicator on home page.

--- app/client/components/map.js
+++ app/client/components/map.js
@@ -4,7 +4,14 @@ class MapContainer extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      status: null
+    };
+
     this.initMap = this.initMap.bind(this);
+    this.reset = this.reset.bind(this);
+    this.lookup = this.lookup.bind(this);
   }
 
   initMap() {
@@ -18,20 +25,36 @@ class MapContainer extends React.Component {
         const myLatlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
         map.setCenter(myLatlng);
         this.props.updateCoords(position.coords.longitude, position.coords.latitude);
+        this.lookup(position.coords.longitude, position.coords.latitude);
       });
     }
 
     map.addListener('click', (location) => {
       const lon = location.latLng.lng();
       const lat = location.latLng.lat();
-      fetch(`/api/lookup?lon=${lon}&lat=${lat}`)
-        .then((response) => response.json())
-        .then((response) => {
-          console.log(response);
-          alert(response.item.result.label);
-          this.props.updateCoords(lon, lat);
-        }
+      
+      this.lookup(lon, lat);
+    });
+  }
+
+  lookup(lon, lat) {
+
+    fetch(`/api/lookup?lon=${lon}&lat=${lat}`)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        this.setState({
+          status: response.item.result.label
+        });
+
+        this.props.updateCoords(lon, lat);
+      }
       )
+  }
+
+  reset() {
+    this.setState({
+      status: null
     });
   }
 
@@ -40,7 +63,14 @@ class MapContainer extends React.Component {
   }
 
   render() {
-    return <div class='map' id='map'></div>
+    return <div>
+      <div className={'map ' + (this.state.status === null ? 'front' : '')} id='map'></div>
+      <div className={'answer ' + (this.state.status !== null ? 'front' : '')} id='answer'>
+        <p>{this.state.status}</p>
+        <button onClick={this.reset}>Back</button>
+      </div>
+    </div> 
+    
   }
 }
 
