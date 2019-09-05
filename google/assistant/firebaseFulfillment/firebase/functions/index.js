61d2a8c2fc95c4f4b30e1d6d945309088596b12f
smulrenn <stephen.mulrennan@oracle.com>
2019-09-05 15:22:04 +0100

    Add firebase functions and logo for deploying action.

--- google/assistant/firebaseFulfillment/firebase/functions/index.js
+++ google/assistant/firebaseFulfillment/firebase/functions/index.js
@@ -1,19 +1,24 @@
 'use strict';
 
 // Import the Dialogflow module from the Actions on Google client library.
-const { dialogflow, Permission } = require('actions-on-google');
+const {
+  dialogflow,
+  Permission
+} = require('actions-on-google');
 
 // Import the firebase-functions package for deployment.
 const functions = require('firebase-functions');
 
 // Instantiate the Dialogflow client.
-const app = dialogflow({ debug: true });
+const app = dialogflow({
+  debug: true
+});
 
 const fetch = require('node-fetch');
 
 const getLocation = (conv) => {
   const permissions = ['NAME'];
-  const context = 'First I need to know your location';
+  const context = 'Hello';
 
   if (conv.user.verification === 'VERIFIED') {
     // Could use DEVICE_COARSE_LOCATION instead for city, zip code
@@ -29,10 +34,16 @@ const getLocation = (conv) => {
 
 // Handle the Dialogflow intent named 'favorite color'.
 // The intent collects a parameter named 'color'.
-app.intent('favorite color', (conv, { color }, confirmationGranted) => {
+app.intent('favorite color', (conv, {
+  color
+}, confirmationGranted) => {
 
-  const { location } = conv.device;
-  const { name } = conv.user;
+  const {
+    location
+  } = conv.device;
+  const {
+    name
+  } = conv.user;
 
   if (location && name) {
     const lng = location.coordinates.longitude;
@@ -42,15 +53,14 @@ app.intent('favorite color', (conv, { color }, confirmationGranted) => {
 
     console.log('Calling ' + url);
 
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
       fetch(url)
         .then(response => response.json())
         .then(response => {
           if (response.item.result.label === 'coat') {
-            conv.close('Goodbye ' + name.given + ' you should wear a coat in ' + response.query.city);
-          }
-          else {
-            conv.close('Goodbye ' + name.given + ' you don\'t need a coat today in ' + response.query.city + ', enjoy the good weather');
+            conv.close('Hi ' + name.given + '. You should wear a coat today in ' + response.query.city + '. Have a nice day');
+          } else {
+            conv.close('Hi ' + name.given + '. You don\'t need a coat today in ' + response.query.city + ', enjoy the good weather');
           }
 
           resolve();
@@ -58,8 +68,7 @@ app.intent('favorite color', (conv, { color }, confirmationGranted) => {
     });
 
 
-  }
-  else {
+  } else {
     getLocation(conv);
   }
 });
